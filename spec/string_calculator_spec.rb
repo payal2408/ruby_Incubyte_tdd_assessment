e70a53fa31b98c07f3e6f4801a2eb7edbb5c8631
Payal Sharma <payal240820@gmail.com>
2024-06-29 22:57:53 +0530

    Handle two numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -9,4 +9,13 @@ RSpec.describe StringCalculator do
     calculator = StringCalculator.new
     expect(calculator.add("1")).to eq(1)
   end
+  it 'returns the sum of two numbers' do
+    calculator = StringCalculator.new
+    expect(calculator.add("1,5")).to eq(6)
+  end
+
+  it 'returns the sum of two numbers in string' do
+    calculator = StringCalculator.new
+    expect(calculator.add("a,5")).to eq(5)
+  end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -2,5 +2,7 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
     return numbers.to_i if numbers.to_i.to_s == numbers
+
+    numbers.split(',').map(&:to_i).sum
   end
 end
